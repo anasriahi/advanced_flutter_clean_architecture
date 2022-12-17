59b23504ae2eb5ecb492b78ad2c1ee83c83799f1
Anas Riahi <anasoasis41@gmail.com>
2022-12-17 03:12:26 +0100

    Initial commit and create Application class

---
+++ android/app/src/main/kotlin/com/riahi/advanced_flutter_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.riahi.advanced_flutter_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // named constructor
+  MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
