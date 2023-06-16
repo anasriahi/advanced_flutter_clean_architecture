f1fd09517a6cf0cc113900855928574ca8379bf7
Anas Riahi <anasriahi.dev@gmail.com>
2023-06-16 17:40:38 +0100

    - Adding dio factory and Logger interceptor. - Adding Language manager. - Update dependencies.

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,7 @@
 class Constants {
   static const String baseUrl = "https://anasriahi.wiremockapi.cloud";
   static const String empty = "";
+  static const String token = "SEND TOKEN HERE";
   static const int zero = 0;
+  static const Duration apiTimeOut = Duration(seconds: 6);
 }
---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,42 @@
+
+import 'package:advanced_flutter_clean_architecture/app/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constants.token,
+      DEFAULT_LANGUAGE: "en"
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: Constants.baseUrl,
+      headers: headers,
+      receiveTimeout: Constants.apiTimeOut,
+      sendTimeout: Constants.apiTimeOut
+    );
+
+    if (!kReleaseMode) { // its debug mode so print app logs
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true
+      ));
+    }
+
+    return dio;
+  }
+}
