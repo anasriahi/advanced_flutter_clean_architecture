9d0b892388aebef8d3cca29b1c48521288f4c916
Anas Riahi <anasriahi.dev@gmail.com>
2023-08-07 16:31:09 +0100

    Adding implementation for (Loading state, Error state, Empty state, Content state) Cases

--- lib/presentation/common/state_renderer/state_renderer_impl.dart
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -1,6 +1,8 @@
 import 'package:advanced_flutter_clean_architecture/app/constants.dart';
 import 'package:advanced_flutter_clean_architecture/presentation/common/state_renderer/state_renderer.dart';
 import 'package:advanced_flutter_clean_architecture/presentation/resources/strings_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 abstract class FlowState {
   StateRendererType getStateRendererType();
@@ -13,7 +15,8 @@ class LoadingState extends FlowState {
   StateRendererType stateRendererType;
   String? message;
 
-  LoadingState({required this.stateRendererType, String message = AppStrings.loading});
+  LoadingState(
+      {required this.stateRendererType, String message = AppStrings.loading});
 
   @override
   String getMessage() => message ?? AppStrings.loading;
@@ -38,7 +41,6 @@ class ErrorState extends FlowState {
 
 // Content State
 class ContentState extends FlowState {
-
   ContentState();
 
   @override
@@ -58,5 +60,85 @@ class EmptyState extends FlowState {
   String getMessage() => message;
 
   @override
-  StateRendererType getStateRendererType() => StateRendererType.fullScreenEmptyState;
+  StateRendererType getStateRendererType() =>
+      StateRendererType.fullScreenEmptyState;
+}
+
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget(BuildContext context, Widget contentScreenWidget,
+      Function retryActionFunction) {
+    switch (runtimeType) {
+      case LoadingState:
+        {
+          if (getStateRendererType() == StateRendererType.popupLoadingState) {
+            // show popup loading
+            showPopup(context, getStateRendererType(), getMessage());
+            // show content ui of the screen
+            return contentScreenWidget;
+          } else {
+            // full screen loading state
+            return StateRenderer(
+                stateRendererType: getStateRendererType(),
+                message: getMessage(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case ErrorState:
+        {
+          dismissDialog(context);
+          if (getStateRendererType() == StateRendererType.popupErrorState) {
+            // show popup error
+            showPopup(context, getStateRendererType(), getMessage());
+            // show content ui of the screen
+            return contentScreenWidget;
+          } else {
+            // full screen loading state
+            return StateRenderer(
+                stateRendererType: getStateRendererType(),
+                message: getMessage(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case EmptyState:
+        {
+          return StateRenderer(
+              stateRendererType: getStateRendererType(),
+              message: getMessage(),
+              retryActionFunction: () {});
+        }
+      case ContentState:
+        {
+          dismissDialog(context);
+          return contentScreenWidget;
+        }
+      default:
+        {
+          dismissDialog(context);
+          return contentScreenWidget;
+        }
+    }
+  }
+
+  _isCurrentDialogShowing(BuildContext context) =>
+      ModalRoute.of(context)?.isCurrent != true;
+
+  dismissDialog(BuildContext context) {
+    if (_isCurrentDialogShowing(context)) {
+      Navigator.of(context, rootNavigator: true).pop(true);
+    }
+  }
+
+  showPopup(BuildContext context, StateRendererType stateRendererType, String message) {
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+        showDialog(
+            context: context,
+            builder: (BuildContext context) =>
+                StateRenderer(
+                    stateRendererType: stateRendererType,
+                    message: message,
+                    retryActionFunction: () {})
+        )
+    );
+  }
+
 }
